d4125dd1d86308b462a3d10b212578cc9adefe2b
Racines-mauricio <mmracines20B@udenar.edu.co>
2025-05-16 16:03:35 -0500

    creando carpeta register y clases create e index

--- server/commons/db.php
+++ server/commons/db.php
@@ -3,11 +3,7 @@
 $host = 'localhost';
 $port = '5433';
 $user = 'postgres';
-<<<<<<< HEAD
 $pass = '1234';
-=======
-$pass = '1004612579';
->>>>>>> 58876d9be71e26fd479cd4d5ac02badafb1113df
 $db_name = 'register_fuel';
 
 
